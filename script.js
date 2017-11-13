4964de5b17baac2c7115cdb54ce990024868dc4e
sarcino <sarka.sevci@gmail.com>
2017-11-13 22:02:55 +0100

    click function

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 window.onload = function() {
 // e = event
     window.addEventListener("keydown", function(e) {
-        const audio = document.querySelector("audio[data-key='" + e.keyCode + "']");
-        const allKeys = document.querySelectorAll("div[data-key='" + e.keyCode + "']");
+        // play the number of the keyboard
+     play(e.keyCode); 
+    });
+
+var keys = document.querySelectorAll(".key");
+keys.forEach(key => {
+    key.addEventListener("transitionend", removeTransition);
+    key.addEventListener("click", function(e) {
+        play(this.getAttribute('data-key'));
+    });
+   });
+};
+
+function play(song) {
+   const audio = document.querySelector("audio[data-key='" + song + "']");
+        const allKeys = document.querySelectorAll("div[data-key='" + song + "']");
         //if there is no audio stop the fuction
         if (!audio) {
             return false;
@@ -11,16 +25,10 @@ window.onload = function() {
         //else play audio
         audio.play();
         
-       allKeys.forEach(key => key.classList.add("playing"));
-        
-    });
+       allKeys.forEach(key => key.classList.add("playing")); 
+}
 
 function removeTransition(e) {
     // skip it if it is not a transform
     this.classList.remove("playing");
-};
-
-var keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
-   
 };
